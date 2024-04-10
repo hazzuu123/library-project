9737dcadf9e9adaeaa17b0d0d1b80d8e40d7f8cd
하주영 <ju980304@naver.com>
2024-04-10 22:13:46 +0900

    Feat: 로그인, 이메일 확인, 패스워드 API 핸들러 구현

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,6 +1,10 @@
 import { StatusCodes } from "http-status-codes";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 import conn from "../mysql.js";
+
+dotenv.config();
 
 export const join = (req, res) => {
   const { email, password } = req.body;
@@ -18,15 +22,77 @@ export const join = (req, res) => {
 };
 
 export const login = (req, res) => {
-  res.status(200).json({ message: "로그인 완료 " });
+  const { email, password } = req.body;
+
+  const sql = `SELECT * FROM  users WHERE email = ?`;
+  const values = [email];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "DB 오류입니다." });
+    }
+    const loginUser = results[0];
+    if (!loginUser || loginUser.password !== password) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "아이디 혹은 비밀번호가 틀렸습니다." });
+    }
+    // 성공 시 로직
+    const token = jwt.sign(
+      { id: loginUser.id, email: loginUser.email },
+      process.env.PRIVATE_KEY,
+      { expiresIn: "5m", issuer: "juyeong" }
+    );
+
+    res.cookie("token", token, { httpOnly: true });
+
+    return res.status(StatusCodes.OK).json({ message: "로그인 완료" });
+  });
 };
 
 export const checkEmail = (req, res) => {
-  res.status(200).json({ message: "이메일 확인 완료" });
+  const { email } = req.body;
+
+  const sql = `SELECT * FROM  users WHERE email = ?`;
+  const values = [email];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "DB 오류입니다." });
+    }
+
+    const user = results[0];
+
+    if (!user) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "아이디가 틀렸습니다." });
+    }
+    return res.status(200).json({ email: user.email });
+  });
 };
 
 export const resetPassword = (req, res) => {
-  res.status(200).json({ message: "비밀번호 변경 완료 " });
+  const { email, password } = req.body;
+
+  const sql = `UPDATE users SET password = ? WHERE email = ?`;
+  const values = [password, email];
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "DB 오류입니다." });
+    }
+
+    if (results.affectedRows === 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "이메일이 없습니다." });
+    }
+    res.status(StatusCodes.OK).json({ message: "비밀번호 변경 완료" });
+  });
 };
 
 export const logout = (req, res) => {
