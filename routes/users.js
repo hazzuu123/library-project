87f0cfae9a95a2b9128c846a61d68cada24503c5
하주영 <ju980304@naver.com>
2024-04-10 19:35:22 +0900

    Feat: express-validator를 사용하여 request 유효성 검사 로직 추가
    - 유효성 검사 실패 시 동작하는 미들웨어 분리
    - 회원가입 API에 적용

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,30 @@
 import express from "express";
+import { Result, body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/join", (req, res) => {
-  res.status(201).json({ message: "회원가입 완료 " });
-});
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    return res.status(400).json(err.array());
+  }
+  return next();
+};
+
+router.post(
+  "/join",
+  [
+    body("email").notEmpty().isString().withMessage("문자열로 입력해주세요."),
+    body("password")
+      .notEmpty()
+      .isString()
+      .withMessage("문자열로 입력해주세요."),
+    validate,
+  ],
+  (req, res) => {
+    res.status(201).json({ message: "회원가입 완료 " });
+  }
+);
 
 router.post("/login", (req, res) => {
   res.status(200).json({ message: "로그인 완료 " });
